9d5c284f98c68a465d5707de8393b8a77a6286a9
kyle <kforker1@gmail.com>
2022-11-29 20:10:26 -0500

    Create lease_confirm.php

---
+++ php/lease_confirm.php
@@ -0,0 +1,38 @@
+<?php session_start();
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+  header("location: login.php");
+  exit;  
+}
+
+require_once "config.php";
+require "password.php";
+
+	$session_user = $_SESSION["username"];
+	$server = "*******";
+  	$user = "*******";
+  	$pass = "*******";
+  	$database = "*******";
+  	$connect = mysql_connect($server, $user, $pass);
+  	mysql_select_db($database);
+
+  	$idSelected = $_POST['selectedApart'];
+  	$results = mysql_query("SELECT APART_ID, BED_TYPE, BATH_TYPE, SQ_FT, PRICE FROM apartments WHERE id = '$idSelected'");
+
+  	$information = mysql_fetch_row($results);
+  	$apart_id = $information[0];
+  	$bed_type = $information[1];
+  	$bath_type = $information[2];
+  	$sq_ft = $information[3];
+  	$price = $information[4];
+
+  	$sql = "INSERT INTO owned (USERNAME, APART_ID, BED_TYPE, BATH_TYPE, SQ_FT, PRICE_RANGE) values ('$session_user', '$apart_id', '$bed_type', '$bath_type', '$sq_ft', '$price')";
+  	mysql_query($sql, $connect);
+
+  	$update = mysql_query("UPDATE apartments SET AVAILABILITY = 'Occupied' WHERE id = '$idSelected'");
+
+  	mysql_close($connect);
+
+  	header("location: index.php");
+
+?>
